e6c1ebbc8451255fa17affdcef6ea2bf699bfe43
Yana <yanaegorova69.3@gmail.com>
2022-09-28 02:44:14 +0300

    +

--- js/templates/product-item.js
+++ js/templates/product-item.js
@@ -1,24 +1,25 @@
-export const item = (product) => {
 
+
+export const item = (product) => {
   return `
     <li class="item" data-aos="fade-up">
-        <div class="product__block js_product glass glass-m" id="${product.id}">
-          <img src="./img/bg3.png" alt="" class="img-1">
-          <img src="./img/bg4.png" alt="" class="img-2">
-          <img src="./img/bg5.png" alt="" class="img-3">
-          <img src="./img/bg6.png" alt="" class="img-4">
-        
-            <div class="product__img-box">
-                <img src="${product.image}" alt="" class="img">
-            </div>
-            <div class="product__text-box">
-                <p class="product__name js_product-name">${product.name}</p>
-                <span class="product__price">$${(product.price).toFixed(2)}</span>
-            </div>
-            <a href="product-page.html?id=${product.id}" class="btn btn--first">product details</a>
-            <button class="btn js_add-to-cart">add to cart</button>
-        </div>
-    </li>`
-}
-
-
+    <div class="product__block js_product glass glass-m" id="${product.id}">
+    <img src="./img/bg3.png" alt="" class="img-1">
+    <img src="./img/bg4.png" alt="" class="img-2">
+    <img src="./img/bg5.png" alt="" class="img-3">
+    <img src="./img/bg6.png" alt="" class="img-4">
+    <div class="product__img-box">
+     <img src="${product.image}" alt="" class="img">
+    </div>
+    <div class="product__text-box">
+    <p class="product__name js_product-name">${product.name}</p>
+      <span class="product__price">$${(product.price).toFixed(2)}</span>
+    </div>
+    <a href="product-page.html?id=${product.id}" class="btn btn--first" style="margin: ${product.isItRing ? 'auto' : ''}">details</a>
+    ${
+      product.isItRing ? '' : '<button class="btn js_add-to-cart">buy</button>'
+    }
+    </div>
+    </li>
+    `
+  }
--- js/website-data.js
+++ js/website-data.js
@@ -16,7 +16,16 @@ let websiteAddress = document.querySelectorAll('.js_website-address');
 let websiteReturnAddress = document.querySelectorAll('.js_website-return-address');
 let websiteDescriptor = document.querySelectorAll('.js_website-desc');
 
-
+const mainPageTitle = document.querySelector('.js_main-title');
+const mainPageSubTitle = document.querySelector('.js_sub-title');
+const MAIN_TAGLINE = 'Selling only the best things online';
+const SECONDARY_TAGLINE = 'Online shopping is the best way to buy everything you need in two clicks.';
+if(mainPageTitle) {
+    mainPageTitle.textContent = MAIN_TAGLINE;
+}
+if(mainPageSubTitle) {
+    mainPageSubTitle.textContent = SECONDARY_TAGLINE;
+}
 
 
 websiteName = [...websiteName];
